6163d99c7ab8634e0bcc01e13cf2c9ecc6889482
Radhika Mattoo <rm3485@nyu.edu>
2016-12-01 16:37:54 -0500

    hotfix for database

--- app/lostnfound/models.py
+++ app/lostnfound/models.py
@@ -2,14 +2,14 @@
 #Radhika Mattoo, @radhikamattoo, rm3485@nyu.edu
 #Lost-n-Found Service
 #Django Models
-from __future__ import unicode_literals
-from django.db import models
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.conf import settings
+from django.db import models
+from django.forms import ModelForm, TextInput
 
-
-
-class Item(models.model):
-    owner = models.ForeignKey('Owner', User)
-    code = models.CharField('Code')
-    qr_code = models.CharField('Code')
+class Item(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='Owner')
+    found = models.BooleanField('Found', default=False)
+    qr_code = models.CharField('Code', max_length=1000)
     name = models.CharField('Name', max_length=40)
