688b5932b7de4a4b4a58f10c07b88ac98fa8864f
Radhika Mattoo <rm3485@nyu.edu>
2016-12-01 16:17:45 -0500

    updating models on master branch

--- app/lostnfound/models.py
+++ app/lostnfound/models.py
@@ -1,5 +1,14 @@
-from __future__ import unicode_literals
-
-from django.db import models
-
 # Create your models here.
+#Radhika Mattoo, @radhikamattoo, rm3485@nyu.edu
+#Lost-n-Found Service
+#Django Models
+from __future__ import unicode_literals
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+class Item(models.model):
+    owner = models.ForeignKey('Owner', User)
+    code = models.CharField('Code')
+    name = models.CharField('Name', max_length=40)
