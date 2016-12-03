ed8789b46296d4251ab11937329867db337c0ded
Revathi Krishnan <rk1998@nyu.edu>
2016-12-03 15:09:15 -0500

    update

--- app/lostnfound/admin.py
+++ app/lostnfound/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+#below subject to change based on change to models.py
+from lostnfound.models import User, Item
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Item)
